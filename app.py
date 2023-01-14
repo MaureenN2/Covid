4e0f9f48eccad417a7eb801a4408bbe4ef4451ea
MaureenN2 <nyambumaureen@gmail.com>
2023-01-14 17:09:56 +0300

    Update

--- app.py
+++ app.py
@@ -36,7 +36,19 @@ covid['Death_change'] = covid['Cumulative_deaths'] - covid['Cumulative_deaths'].
 # creating hover data for daily percentage growth rate in deaths
 covid['Death_Daily_change'] = np.round(covid['Cumulative_deaths'].pct_change()*100, 2).astype(str) + '%'
 
+#Getting cumulative deaths
+total_deaths = 0
+for deaths in covid['New_deaths']:
+    total_deaths += deaths
+    
+print(total_deaths)
 
+#Getting cumulative deaths
+total_cases = 0
+for cases in covid['New_cases']:
+    total_cases += cases
+    
+print(total_cases)
 
 # creating a select options for analysis & visualization in choropleth
 if selection== 'New_cases':
@@ -47,6 +59,10 @@ if selection== 'New_cases':
     color_continuous_scale=px.colors.sequential.Peach,
     hover_name = 'Country', 
     hover_data = ['Cumulative_cases', 'New_deaths', 'Cumulative_deaths'])
+    fig.update_layout(height=550) 
+    st.plotly_chart(fig, use_container_width=True, theme='streamlit')
+    st.write('United States has the highestcumulative deaths at 1.07M followed by Brazil at 694K, India 530K, Russia 394K and Mexico 331K')
+
 elif selection== 'Cumulative_cases':
     st.header('Covid Cumulative Cases')
     fig = px.choropleth(covid, locations = 'iso_alpha', 
@@ -55,6 +71,10 @@ elif selection== 'Cumulative_cases':
     color_continuous_scale=px.colors.sequential.Mint,
     hover_name = 'Country', 
     hover_data = ['New_cases', 'New_deaths', 'Cumulative_deaths'])
+    fig.update_layout(height=550) 
+    st.plotly_chart(fig, use_container_width=True, theme='streamlit')
+    st.write('United States has the highest cumulative cases at 99M followed by India 44M, France 38M, Germany 37M, Brazil 36M')
+
 elif selection == 'New_deaths':
     st.header('Covid New Deaths')
     fig = px.choropleth(covid, locations = 'iso_alpha', 
@@ -63,6 +83,10 @@ elif selection == 'New_deaths':
     color_continuous_scale=px.colors.sequential.Peach,
     hover_name = 'Country', 
     hover_data = ['New_cases', 'Cumulative_cases', 'Cumulative_deaths'])
+    fig.update_layout(height=550) 
+    st.plotly_chart(fig, use_container_width=True, theme='streamlit')
+    st.write('First new death was reported on Jan 12 2020 in China. As of Jan 9th 2023 Japan had the highest new death 407 followed by China 107')
+
 else:
     st.header('Covid Cumulative Deaths')
     fig = px.choropleth(covid, locations = 'iso_alpha', 
@@ -70,26 +94,31 @@ else:
     animation_frame = 'Date_reported',
     color_continuous_scale=px.colors.sequential.Mint,
     hover_name = 'Country')
-    # st.sidebar.markdown('_Highest cumulative deaths are from:_')
-    # st.sidebar.write(['Unites states; 1M, Brazil; 694K, India; 530k'])
-    # hover_data = ['New_cases', 'Cumulative_cases']
-fig.update_layout(height=550) 
-st.plotly_chart(fig, use_container_width=True, theme='streamlit')
+    fig.update_layout(height=550) 
+    st.plotly_chart(fig, use_container_width=True, theme='streamlit')
+    st.markdown('_Highest cumulative deaths are from:_')
+    st.write(['Unites states; 1M, Brazil; 694K, India; 530k'])
 
 
 #Bar graphs for cumulative cases & deaths
 col1, col2 = st.columns(2)
 
 with col1:
+    st.header('Global Situation')
+    st.write('<h2 style="text-align:center; font-weight:bold;">' + str(total_cases) + '<br><span style="text-decoration:none; font-weight:normal; font-size:30px">Confirmed Cases</span></h2>', unsafe_allow_html=True)
+
+with col2:        
     st.header('Cumulative Cases per Day Overview')
     fig2 = px.bar(covid, x = 'Date_reported', y = 'Cumulative_cases', hover_data = ['cases', 'Cases_change', 'Cases_Daily_change'], title = 'Cumulative Cases')
-    fig2.update_layout(400)
     st.plotly_chart(fig2, use_container_width=True, theme='streamlit')
+    
+col3, col4 = st.columns(2)
 
-with col2:
+with col3:
+    st.write('<h2 style="text-align:center; font-weight:bold;">' + str(total_deaths) + '<br><span style="text-decoration:none; font-weight:normal; font-size:30px">Deaths</span></h2>', unsafe_allow_html=True)
+
+
+with col4:
     st.header('Cumulative Deaths per Day Overview')
     fig3 = px.bar(covid, x = 'Date_reported', y = 'Cumulative_deaths', hover_data = ['deaths', 'Death_change', 'Death_Daily_change'], title = 'Cumulative deaths')
-    fig3.update_layout(400)
     st.plotly_chart(fig3, use_container_width=True, theme='streamlit')
-
-
